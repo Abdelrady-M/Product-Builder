1e9344c9f54232196c8199bd1688f272ac385e82
Abdelrady <ahmedmc95@gmail.com.com>
2024-04-26 14:17:36 +0300

    ReactJs + Ts

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,19 @@
+import { ButtonHTMLAttributes, ReactNode } from "react"
+
+
+interface  IProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+children: ReactNode,
+className?: string
+width?:'w-full' | 'w-fit';
+}
+
+
+const Button = ({children, className, width = "w-full", ...rest}: IProps) => {
+  return (
+    <button   className={`${className} ${width}  text-white rounded-lg w-full p-2 `}  {...rest}>
+        {children} 
+    </button>
+  )
+}
+
+export default Button
---
+++ src/validation/index.tsx
@@ -0,0 +1,43 @@
+
+
+
+
+
+export const productValidation = (product:{ 
+    title: string;
+    description: string;
+    imageURL: string;
+    price: string;
+})=>{
+    //** Return an object  
+const errors: {
+    title: string;
+    description: string;
+    imageURL: string;
+    price: string;
+    } =
+    {
+        title: "",
+        description: "",
+        imageURL: "",
+        price: "",
+    }
+
+const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(product.imageURL);
+
+
+if (!product.title.trim() || product.title.length < 10 || product.title.length > 80){
+    errors.title = "Title must be between 10 and 80 characters"
+}
+if (!product.description.trim() || product.description.length < 10 || product.description.length > 900){
+    errors.description = "Description must be between 10 and 900 characters"
+}
+if (!product.imageURL.trim() || !validUrl){
+    errors.imageURL = "Please enter a valid image URL"
+}
+
+if (!product.price.trim() || isNaN(Number(product.price))){
+    errors.price = "Please enter a valid price"
+}
+    return errors
+}
